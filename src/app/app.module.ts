0a59bb5443a8e46ced9d0dccf7d36aa663f6310c
Victor Manuel Carmona Moraza <VictorCarmona9035@hotmail.com>
2022-03-31 19:23:27 +0200

    2- Creacion de la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarrasComponent } from './graficas/pages/barras/barras.component';
+import { BarrasDobleComponent } from './graficas/pages/barras-doble/barras-doble.component';
+import { DonaHttpComponent } from './graficas/pages/dona-http/dona-http.component';
+import { GraficaBarraComponent } from './graficas/components/grafica-barra/grafica-barra.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarrasComponent,
+    BarrasDobleComponent,
+    DonaHttpComponent,
+    GraficaBarraComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
