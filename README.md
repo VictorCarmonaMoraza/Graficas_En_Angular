50a826d58e68001437a249b7dfa02db07d0f2d29
Victor Manuel Carmona Moraza <VictorCarmona9035@hotmail.com>
2022-04-03 22:40:19 +0200

    6 - Personalizando la gráfica

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## 5 - Gráfica de barras
+## 6 - Personalizando la gráfica
+
+Hemos cambiado color a las barras de la grafica y hemos introducido una tercera barra
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -19,21 +19,21 @@ export class BarrasComponent{
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10
-      }
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-      datalabels: {
-        anchor: 'end',
-        align: 'end'
-      }
-    }
+    // scales: {
+    //   x: {},
+    //   y: {
+    //     min: 10
+    //   }
+    // },
+    // plugins: {
+    //   legend: {
+    //     display: true,
+    //   },
+    //   datalabels: {
+    //     anchor: 'end',
+    //     align: 'end'
+    //   }
+    // }
   };
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [
@@ -43,9 +43,10 @@ export class BarrasComponent{
   public barChartData: ChartData<'bar'> = {
     labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-    ]
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#52BE80', hoverBackgroundColor:'red' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#BFC9CA', hoverBackgroundColor: 'red' },
+      { data: [55, 37, 69, 25, 12, 52, 61], label: 'Series C', backgroundColor: '#EC7063', hoverBackgroundColor: 'red' }
+    ],
   };
 
   // events
@@ -67,6 +68,25 @@ export class BarrasComponent{
       56,
       Math.round(Math.random() * 100),
       40];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      28,
+      48,
+      Math.round(Math.random() * 100),
+      86,
+      Math.round(Math.random() * 100),
+      90];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      55,
+      37,
+      69,
+      Math.round(Math.random() * 100),
+      25,
+      Math.round(Math.random() * 100),
+      12];
 
     this.chart?.update();
   }
